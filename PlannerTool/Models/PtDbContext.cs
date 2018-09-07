f6ba528fbf6399e5b98bc293dcb6f08700094fb4
Jonathan <jon.wilsond@gmail.com>
2018-09-07 12:14:27 -0400

    added users and entity framework

---
+++ PlannerTool/Models/PtDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PlannerTool.Models
+{
+    public class PtDbContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public PtDbContext() : base() { }
+    }
+}
--- PlannerTool/Models/User.cs
+++ PlannerTool/Models/User.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,37 @@ namespace PlannerTool.Models
 {
     public class User
     {
+        public int Id { get; set; }
+        [Required]
+        //[Index(IsUnique = true)]
+        [StringLength(30)]
+        public string UserName { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string Password { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string LastName { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(12)]
+        public string Phone { get; set; }
+        [Required]
+        public bool IsReviewer { get; set; } = false;
+        [Required]
+        public bool IsAdmin { get; set; } = false;
+        [Required]
+        public bool Active { get; set; } = true;
+
+
+        public User()
+        {
+
+        }
     }
 }
